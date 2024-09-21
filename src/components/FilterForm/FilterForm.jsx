25055ee3c93a9522c3e64c405404fb1126bd65d4
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-09-21 17:12:46 +0300

    fix form UI

--- src/components/FilterForm/FilterForm.jsx
+++ src/components/FilterForm/FilterForm.jsx
@@ -1,5 +1,7 @@
 import { Formik, Field, Form, FieldArray } from 'formik';
 import Button from '../../shared/Button/Button';
+import { cities } from '../../helpers/citiesList';
+import clsx from 'clsx';
 import css from '../FilterForm/FilterForm.module.css';
 
 export default function FilterForm() {
@@ -8,45 +10,75 @@ export default function FilterForm() {
     console.log(value);
   };
 
-  const equipmentOptions = [
-    { value: 1 },
-    { value: 2 },
-    { value: 3 },
-    { value: 4 },
-    { value: 5 },
-  ];
-
   return (
     <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-      <Form>
+      <Form className={css.form}>
+        <datalist id="city-options">
+          {cities.map((city, index) => (
+            <option key={index} value={city} />
+          ))}
+        </datalist>
         <label className={css.locationLabel}>
           Location
           <Field
             className={css.locationInput}
             name="location"
             placeholder="Kyiv, Ukraine"
+            list="city-options"
           />
         </label>
-
         <p className={css.text}>Filters</p>
-
         <FieldArray
           name="equipment"
           render={() => (
-            <div>
-              <label>Vehicle equipment</label>
-              {equipmentOptions.map((equipment, index) => (
-                <Field
-                  key={index}
-                  type="checkbox"
-                  name="equipment"
-                  value={equipment.value.toString()}
-                />
-              ))}
+            <div className={css.equipmentContainer}>
+              <p className={css.textBold}>Vehicle equipment</p>
+              <hr className={css.string} />
+              <div className={css.equipmentWrapper}>
+                <label className={css.box}>
+                  <Field
+                    className={css.checkbox}
+                    type="checkbox"
+                    name="equipment"
+                    value={'1'}
+                  />
+                </label>
+                <label className={css.box}>
+                  <Field
+                    className={css.checkbox}
+                    type="checkbox"
+                    name="equipment"
+                    value={'2'}
+                  />
+                </label>
+                <label className={css.box}>
+                  <Field
+                    className={css.checkbox}
+                    type="checkbox"
+                    name="equipment"
+                    value={'3'}
+                  />
+                </label>
+                <label className={css.box}>
+                  <Field
+                    className={css.checkbox}
+                    type="checkbox"
+                    name="equipment"
+                    value={'4'}
+                  />
+                </label>
+                <label className={css.box}>
+                  <Field
+                    className={css.checkbox}
+                    type="checkbox"
+                    name="equipment"
+                    value={'5'}
+                  />
+                </label>
+              </div>
             </div>
           )}
         />
-
         <div id="type">Picked</div>
         <div role="group" aria-labelledby="my-radio-group">
           <label>
@@ -62,7 +94,6 @@ export default function FilterForm() {
             Three
           </label>
         </div>
-
         <Button text="Search" />
       </Form>
     </Formik>
---
+++ src/helpers/citiesList.js
@@ -0,0 +1,7 @@
+export  const cities = ['Kyiv, Ukraine',
+  'Lviv, Ukraine',
+  'Odessa, Ukraine',
+  'Kharkiv, Ukraine',
+  'Dnipro, Ukraine',
+    'Ivano-Frankivsk, Ukraine',
+'Poltava, Ukraine']
