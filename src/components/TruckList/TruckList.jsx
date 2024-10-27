c866110cf75d7525a82311fd911863741952402f
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-10-28 00:02:50 +0200

    add axios

--- src/components/TruckList/TruckList.jsx
+++ src/components/TruckList/TruckList.jsx
@@ -1,7 +1,11 @@
 import TruckCard from '../TruckCard/TruckCard';
+import { getTrucks } from '../../API/apiOperations.js';
 import css from '../TruckList/TruckList.module.css';
 
 export default function TruckList() {
+  const arr = getTrucks();
+  console.log(arr);
+
   return (
     <ul className={css.list}>
       <li>
