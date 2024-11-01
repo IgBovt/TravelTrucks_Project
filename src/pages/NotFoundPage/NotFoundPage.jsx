660ada6836b729ee02fb1d886cf67b9ab134d81e
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-11-01 19:38:31 +0200

    made not found page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,10 @@
+import css from '../NotFoundPage/NotFoundPage.module.css';
+import Icon from '../../shared/Icon/Icon';
 export default function NotFoundPage() {
-  return <p>NotFoundPage</p>;
+  return (
+    <div className={css.container}>
+      <Icon className={css.icon} width="48" height="48" id="icon-sad" />
+      <p>Page not found...</p>
+    </div>
+  );
 }
