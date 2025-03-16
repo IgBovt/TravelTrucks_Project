ba721764258bc18d585f8c7aec4e142cf0c53e76
IgBovt <140014814+IgBovt@users.noreply.github.com>
2025-03-16 19:38:58 +0200

    made favorites

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import truckReducer from './trucks/slice'
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const truckPersistConfig = {
+  key: 'trucks',
+  storage,
+  whitelist: ['favoritesTrucks']
+}
 
+const persistedTruckReducer = persistReducer(truckPersistConfig, truckReducer)
 
 export const store = configureStore({
   reducer: {
-    trucks: truckReducer,
+    trucks: persistedTruckReducer,
 
   },
+   middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export const persistor = persistStore(store)
 
